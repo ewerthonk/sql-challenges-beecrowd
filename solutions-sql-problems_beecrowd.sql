225d96662ad8059e4b4958836b65f5069ccb8e60
Giovane Martins <3141734+giovanemartins@users.noreply.github.com>
2022-12-06 17:20:11 -0300

    Update solutions-sql-problems_beecrowd.sql
    Fix on challenge 2996 - missing where and order

--- solutions-sql-problems_beecrowd.sql
+++ solutions-sql-problems_beecrowd.sql
@@ -995,8 +995,9 @@ INNER JOIN users AS senders
      ON packages.id_user_sender = senders.id 
 INNER JOIN users AS receivers
      ON packages.id_user_receiver = receivers.id
-;
-
+WHERE (packages.color='blue' or packages.year=2015) AND 
+(senders.address <> 'Taiwan' AND receivers.address <> 'Taiwan')
+ORDER BY packages.year DESC;
 
 
 -- beecrowd #2997: Employees Payment
